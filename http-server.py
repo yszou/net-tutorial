ad192b8d1327ece20ef863918ecb19f1d4ee0bdc
YS.Zou <yeshengzou@gmail.com>
2021-04-06 19:23:55 +0800

    http server

---
+++ http-server.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+ 
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(('0.0.0.0', 8888))
+sock.listen()
+print(sock.getsockname())
+
+# blocking
+conn, addr = sock.accept()
+
+print('connection from ', addr)
+
+while True:
+    data = conn.recv(1024)
+    data = data.strip()
+    if data == b'quit': break
+    if not data: break
+    print(b'receive: ' + data)
+    http = b'HTTP 200 OK\r\ncontent-type: text/plain\r\ncontent-length: 3\r\n\r\n123'
+    conn.sendall(http)
+    #conn.sendall(b'reply:' + data + b'\n')
+
+
+sock.close()
+
